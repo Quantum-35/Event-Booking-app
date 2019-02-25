9a48c257b7b2a55cbe83828fa697e0c7f3543455
Quantum-35 <quantumcomputing35@icloud.com>
2019-02-25 06:42:15 +0300

    chore(db) setup Mongoose  - setup mongodb and mongoose  - save events to mongo

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const Event = require('./models/event');
 
 
 const app = express();
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -41,19 +44,37 @@ app.use('/graphql', graphqlHttp({
     }
   `),
   rootValue: {
-    events: () => events,
+    events: () => {
+      return Event.find()
+        .then((events) => { 
+          return events.map((event) => {
+            return { ...event._doc, _id: event._doc._id.toString() };
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+          throw err;
+        });
+    },
     createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
+      const event = new Event({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: args.eventInput.date,
-      };
-      events.push(event);
-      return event;
+        date: new Date(args.eventInput.date),
+      });
+      return event.save()
+        .then((result) => {
+          return { ...result._doc }; // mongoose property that return the document object
+        })
+        .catch((err) => {
+          console.log(err);
+          throw err;
+        });
     },
   },
   graphiql: true,
 }));
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
 app.listen(3001, () => console.log('Server running at port 3001'));
