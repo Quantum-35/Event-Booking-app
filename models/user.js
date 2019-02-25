1f593719298c6e617270312fdbe4928aee6cf6c6
Quantum-35 <quantumcomputing35@icloud.com>
2019-02-25 14:36:59 +0300

    feat(user)Add user model  - Add user model  - create a relationship between user and event.

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+// eslint-disable-next-line
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdEvents: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Event',
+    },
+  ],
+});
+
+module.exports = mongoose.model('User', userSchema);
