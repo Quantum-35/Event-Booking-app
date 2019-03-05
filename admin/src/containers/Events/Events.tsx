dc1cafcaf4122d1537500f6396e694e6564d9af2
Quantum-35 <quantumcomputing35@icloud.com>
2019-03-05 21:17:12 +0300

    feat(frontend) Implement authentication  - Implement user login and signup  - Enable user Create Events

--- admin/src/containers/Events/Events.tsx
+++ admin/src/containers/Events/Events.tsx
@@ -1,11 +1,113 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-export default class Events extends Component {
+import { fetchEvent, createEvent  } from './actions';
+import Modal from '../../components/Modal/Modal';
+import BackDrop from '../../components/Backdrop/Backdrop';
+import './Events.css';
+import { async } from 'q';
+
+interface iProps {
+  fetchEvents: (body: any) => void;
+  createEvent: (body: any) => void;
+}
+
+class Events extends Component<iProps> {
+  state = {
+    createEvent: false,
+    title: null,
+    price: null,
+    date: null,
+    description: null
+  }
+
+  componentDidMount() {
+    const { fetchEvents } = this.props;
+    try {
+      fetchEvents(null);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  handleOnChange = (e) => {
+    const {name, value}: any = e.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleCreateEvent = () => {
+    this.setState({createEvent: true});
+  }
+
+  handleCancel = () => {
+    this.setState({createEvent: false});
+  }
+
+  handleConfirm = async() => {
+    const {title, price, date, description} = this.state;
+    const { createEvent } = this.props;
+    if (!title || !price || !date || !description) {
+      console.log('Fields above can not be null');
+      return;
+    }
+    try {
+      console.log(parseFloat(price).toFixed(2));
+      const data = {
+        title,
+        price,
+        date,
+        description
+      }
+      await createEvent(data);
+      this.setState({createEvent: false});
+    } catch (error) {
+      throw error;
+    }
+  }
+
   render() {
+    const { createEvent } = this.state;
     return (
-      <div>
-        <h1>Welcome to Events Page</h1>
-      </div>
+      <React.Fragment>
+        {createEvent && <BackDrop />}
+        {createEvent && <Modal title="Add Event"canCancel canConfirm onCancel={this.handleCancel} onConfirm={this.handleConfirm}>
+          <form className="event-form">
+            <div className="form-control">
+              <label htmlFor="title">Title</label>
+              <input type="text" name="title" id="title" required onChange={this.handleOnChange}/>
+            </div>
+            <div className="form-control">
+              <label htmlFor="price">Price</label>
+              <input type="number" name="price" id="price" required onChange={this.handleOnChange}/>
+            </div>
+            <div className="form-control">
+              <label htmlFor="date">Date</label>
+              <input type="datetime-local" name="date" id="date" required onChange={this.handleOnChange}/>
+            </div>
+            <div className="form-control">
+              <label htmlFor="description">Description</label>
+              <textarea name="description" id="description" cols={85} rows={7} required onChange={this.handleOnChange}></textarea>
+            </div>
+          </form>
+        </Modal>}
+        <div className="events-control">
+          <p>Share your own Events !!!</p>
+          <button className="btn-create-event" onClick={this.handleCreateEvent}>Create event</button>
+        </div>
+      </React.Fragment>
     )
   }
 }
+
+const mapStateToProps = state => ({
+  events: state
+});
+const mapDispatchToProps = dispatch => bindActionCreators({
+  fetchEvents: fetchEvent,
+  createEvent
+}, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Events);
--- admin/src/stores/RootReducer.ts
+++ admin/src/stores/RootReducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
 import authReducer from '../containers/Auth/reducer';
+import eventReducer from '../containers/Events/reducer';
 
 export default combineReducers({
-    authReducer
+    authReducer,
+    eventReducer
 });
